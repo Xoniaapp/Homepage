88bd46216b96c2b1d5879f3dc4238fd79c560bae
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2021-10-28 16:37:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {
-  Home,
-  Pricing,
-  Error,
-  Terms,
-  Privacy,
-  Team
-} from "./pages/index"
-
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+    <div>
+    <center>
+    <h1>
+    Something big is coming...
+    </h1>
+    </center>
+    </div>
         </Route>
-
-        <Route exact path="/xtra">
-          <Pricing />
-        </Route>
-
-        <Route exact path="/terms">
-          <Terms />
-        </Route>
-
-        <Route exact path="/privacy">
-          <Privacy />
-        </Route>
-
-        <Route exact path="/team">
-          <Team />
-        </Route>
-
-        <Route exact path="*">
-          <Error />
-        </Route>
-
       </Switch>
     </Router>
   );
